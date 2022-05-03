c7122e63aa7b23af44dbcf63b57c8adb6611623b
Francisco Martins <francisco.drive.7@gmail.com>
2022-05-03 23:39:27 +0100

    Upgraded beforeException

--- app/plugins/NotFoundPlugin.php
+++ app/plugins/NotFoundPlugin.php
@@ -1,9 +1,9 @@
 <?php
 
 use Phalcon\Di\Injectable;
+use Phalcon\Dispatcher\Exception;
 use Phalcon\Events\Event;
-use Phalcon\Mvc\Dispatcher as MvcDispatcher;
-use Phalcon\Mvc\Dispatcher\Exception as DispatcherException;
+use Phalcon\Mvc\Dispatcher;
 
 /**
  * Plugin that handles unhandled routes
@@ -14,21 +14,19 @@ use Phalcon\Mvc\Dispatcher\Exception as DispatcherException;
 class NotFoundPlugin extends Injectable
 {
 
-    public function beforeException(Event $event, MvcDispatcher $dispatcher, Exception $exception): bool
+    public function beforeException(Event $event, Dispatcher $dispatcher, Exception $exception): bool
     {
         error_log($event->getSource(), $exception->getMessage() . PHP_EOL . $exception->getTraceAsString());
-        if ($exception instanceof DispatcherException) {
-            switch ($exception->getCode()) {
-                case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
-                case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
-                    $dispatcher->forward(
-                        [
-                            'controller' => 'Errors',
-                            'action' => 'show404'
-                        ]
-                    );
-                    return false;
-            }
+        switch ($exception->getCode()) {
+            case Exception::EXCEPTION_HANDLER_NOT_FOUND:
+            case Exception::EXCEPTION_ACTION_NOT_FOUND:
+                $dispatcher->forward(
+                    [
+                        'controller' => 'Errors',
+                        'action' => 'show404'
+                    ]
+                );
+                return false;
         }
         $dispatcher->forward(
             [
